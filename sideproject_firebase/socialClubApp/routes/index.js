d931518cec2cd8b6762be4f6504604b47c2fb841
dovmfgovl <plum0626@daum.net>
2023-11-15 19:56:57 +0900

    project_라우터 depth 깊게 가져가기, clubMain까지

--- sideproject_firebase/socialClubApp/routes/index.js
+++ sideproject_firebase/socialClubApp/routes/index.js
@@ -4,13 +4,17 @@ var router = express.Router(); //페이지 전환
 router.get('/', function(req, res, next) {
   res.render('index', { title: '소소모임', pageName:"home.ejs"});
 });
+router.get('/create', function(req, res, next) {
+  res.render('index', { title: '클럽생성', pageName:"create.ejs"});
+});
 router.get('/clubList/:id', function(req, res, next) {
   let id = req.params.id
   res.render('index', { title: '클럽목록' , pageName: "clubList.ejs", id:id});
 });
-router.get('/clubMain', function(req, res, next) {
-  let id = req.params.id
-  res.render('index', { title: '클럽메인' , pageName: "clubMain.ejs"});
+router.get('/clubMain/:category/:clubId', function(req, res, next) {
+  let category = req.params.category
+  let clubId = req.params.clubId
+  res.render('index', { title: '클럽메인' , pageName: "clubMain.ejs", id:category, id2:clubId});
 });
 // router.get('/login', function(req, res, next) { //app.js -> path 라이브러리 __dirname, views
 //   res.render('index', { title: '로그인', pageName:"auth/login.ejs" });
