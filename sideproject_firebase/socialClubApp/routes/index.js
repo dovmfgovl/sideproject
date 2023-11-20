98fa4d947e853984bc926cc92c84c5249d7fba9a
plum <dovmfgovl@gmail.com>
2023-11-20 10:20:36 +0900

    board, album 구현 중

--- sideproject_firebase/socialClubApp/routes/index.js
+++ sideproject_firebase/socialClubApp/routes/index.js
@@ -22,15 +22,35 @@ router.get('/login', function(req, res, next) {
 router.get('/join', function(req, res, next) { 
   res.render('index', { title: '회원가입', pageName:"auth/join.ejs" });
 });
-// router.get('/board/list', function(req, res, next) { //app.js -> path 라이브러리 __dirname, views
-//   res.render('index', { title: '글목록', pageName:"board/list.ejs" });
-// });
-// router.get('/board/write', function(req, res, next) { //app.js -> path 라이브러리 __dirname, views
-//   res.render('index', { title: '글쓰기', pageName:"board/write.ejs" });
-// });
+router.get('/clubMain/:category/:clubId/board', function(req, res, next) { 
+  let category = req.params.category
+  let clubId = req.params.clubId
+  res.render('index', { title: '글목록', pageName:"clubActivity/board/list.ejs", id:category, id2:clubId });
+});
+router.get('/clubMain/:category/:clubId/board/write', function(req, res, next) { 
+  let category = req.params.category
+  let clubId = req.params.clubId
+  res.render('index', { title: '글쓰기', pageName:"clubActivity/board/write.ejs", id:category, id2:clubId });
+});
+router.get('/clubMain/:category/:clubId/board/:readId', function(req, res, next) { 
+  let category = req.params.category
+  let clubId = req.params.clubId
+  let readId = req.params.readId
+  res.render('index', { title: '글 상세보기', pageName:"clubActivity/board/read.ejs", id:category, id2:clubId, id3:readId });
+});
 // router.get('/board/update/:id', function(req, res, next) { //app.js -> path 라이브러리 __dirname, views
 //   let id = req.params.id;
 //   res.render('index', { title: '글수정', pageName:"board/update.ejs", id: id });
 // });
+router.get('/clubMain/:category/:clubId/album', function(req, res, next) { 
+  let category = req.params.category
+  let clubId = req.params.clubId
+  res.render('index', { title: '앨범목록', pageName:"clubActivity/album/albumList.ejs", id:category, id2:clubId });
+});
+router.get('/clubMain/:category/:clubId/album/upload', function(req, res, next) { 
+  let category = req.params.category
+  let clubId = req.params.clubId
+  res.render('index', { title: '사진업로드', pageName:"clubActivity/album/upload.ejs", id:category, id2:clubId });
+});
 
 module.exports = router;
